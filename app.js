b87024d29bb3fcfb30428ab0621c11653e9f1ee7
Haiyah Perez <haiyahperez@pursuit.org>
2024-02-21 18:12:01 -0500

    done

--- app.js
+++ app.js
@@ -1,43 +1,46 @@
 // DEPENDENCIES
-const express = require('express')
-const cors = require("cors")
-const pokemons = require('./models/pokemon.json')
+const express = require('express');
+const cors = require('cors');
+const pokemons = require('./models/pokemon.json');
 
-console.log(pokemons[0])
+console.log(pokemons[0]);
+
 // CONFIGURATION
-const app = express()
+const app = express();
+const PORT=8888;
 
-//MIDDLEWARE
+// MIDDLEWARE
 app.use(cors());
 
 // ROUTES
 app.get('/', (req, res) => {
-    res.send('Welcome to my Pokemon World')
-})
+    res.send('Welcome to my Pokemon World');
+});
 
 app.get('/pokemon', (req, res) => {
-    res.send(pokemons)
-})
+    res.send(pokemons);
+});
 
 app.get('/pokemon/:indexOfArray', (req, res) => {
-    const { indexOfArray} = req.params
-    
-    if (pokemon[indexOfArray]) {
-        res.json({pokemon: pokeons[indexOfArray]});
+    const { indexOfArray } = req.params;
+
+    if (pokemons[indexOfArray]) {
+        res.json({ pokemon: pokemons[indexOfArray] });
     } else {
-        res.json({ message: `Sorry, no Pokémon found at /pokemon/${indexOfArray}`})
+        res.json({ message: `Sorry, no Pokémon found at /pokemon/${indexOfArray}` });
     }
 });
 
-app.get('/pokemon/search', (res, req) => {
+app.get('/pokemon/search', (req, res) => {
     const { name } = req.query;
-    const findPokemon = pokemons.find(pokemon => pokemon.name.toLowerCase());
-        
+    const findPokemon = pokemons.find(pokemon => pokemon.name.toLowerCase() === name.toLowerCase());
+
     if (!findPokemon) {
-            return res.json({ message: `No Pokémon found with name '${name}'` });
-        } else {
-            res.json({ pokemon: findPokemon });
-        }
+        return res.json({ message: `No Pokémon found with name '${name}'` });
+    } else {
+        res.json({ pokemon: findPokemon });
+    }
 });
+
 // EXPORT
-module.exports = app
+module.exports = app;
