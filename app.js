42d0d1ba4b8c96889249da03f9bbdccaad08ccfb
jadenglenn <jadenglenn@pursuit.org>
2024-02-21 16:16:56 -0500

    lab complete

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const express = require("express")
+const cors = require('cors')
 const pokemon = require('./models/pokemon.json')
 const app = express()
 
-
+app.use(cors())
 
 app.get('/', (req, res) => {
     res.send(`App Initialized`)
